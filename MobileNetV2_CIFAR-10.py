f813916ebb093f938303041c50aab423a28a3868
Sam <meyersamuel.91@gmail.com>
2021-01-26 18:25:22 +0100

    ajout sauvegarde du modèle et prédictions sur les images test

--- MobileNetV2_CIFAR-10.py
+++ MobileNetV2_CIFAR-10.py
@@ -7,13 +7,12 @@ from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 
-batch_size = 64
 
-dataset_name = "CIFAR_10"
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-
 num_classes = 10
-input_shape = (32, 32, 3)
+
+
 
 # Pour voir les images :
 for i in range(9):
@@ -21,24 +20,26 @@ for i in range(9):
     plt.imshow(x_train[i].astype("uint8"))
     plt.title(y_train[i])
     plt.axis("off")
-    
+ 
+#transformation des vecteurs résultats en scalaires
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
-
-
+input_shape = (32, 32, 3)
+batch_size = 64
 target_size = 96
 
-#datagen=keras.
+#création d'un générateur
 datagen=keras.preprocessing.image.ImageDataGenerator(
     rescale=1./255,
     validation_split=0.2)
 
 train_generator=datagen.flow(x_train,y_train,subset='training')
 
+
 def build_model( ):
-    input_tensor=layers.experimental.preprocessing.Resizing(height=96,width=96) ##faut il redimensionner ici ? ça marche ?
+    #input_tensor=layers.experimental.preprocessing.Resizing(height=96,width=96) ##faut il redimensionner ici ? ça marche ?
     input_tensor = layers.Input(shape=(target_size, target_size ,3))
     base_model = tf.keras.applications.MobileNetV2(
         include_top=False,
@@ -63,26 +64,27 @@ model=build_model()
 model.compile(
         optimizer=keras.optimizers.Adam(), loss="categorical_crossentropy", metrics=["accuracy"]
     )
+ 
+epochs = 10
 
-    
-    
-epochs = 10  # @param {type: "slider", min:8, max:80}
+hist=model.fit(train_generator,epochs=epochs,verbose=2)
+
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['loss'])
+
+#Sauvegarder le modèle
+model_name='mobilenetv2_cifar'
+tf.saved_model.save(model,'saved_models/'+ model_name)
 
 
-
-
-model.fit(train_generator,epochs=epochs,verbose=2)
-
-
-#plot_hist(hist)
-
-
+#test de l'image numéro :
+num=5 
 from skimage.transform import resize
-num=5
 x = resize(x_train[num], (target_size, target_size,3))
 x_expanded = np.expand_dims(x, axis=0)
 predictions = model.predict(x_expanded)
-print(predictions)
+print(predictions*100) #améliorer l'affichage des résultats
 
-#results = keras.applications.imagenet_utils.decode_predictions(predictions) #marche pas car pas les mêmes catégories
-
+#Prédictions sur les images test
+test_generator=datagen.flow(x_test,y_test)
+model.evaluate(test_generator,verbose=2)
