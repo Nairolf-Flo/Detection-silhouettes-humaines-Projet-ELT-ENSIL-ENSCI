553a6ef6ab7a3e092747eb4f8519cc69c2d4b8a9
Sam <meyersamuel.91@gmail.com>
2021-02-06 14:34:17 +0100

    Sélection des images provenant de COCO

--- test_coco.py
+++ test_coco.py
@@ -26,13 +26,13 @@ print('COCO supercategories: \n{}'.format(' '.join(nms)))
 # get all images containing given categories, select one at random
 catIds = coco.getCatIds(catNms=['person','dog','skateboard']);
 imgIds = coco.getImgIds(catIds=catIds );
-imgIds = coco.getImgIds(imgIds = [324158])
+#imgIds = coco.getImgIds(imgIds = [324158])
 img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-
+images = coco.loadImgs(imgIds)
 # load and display image
-# I = io.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
+I = io.imread('%s/images/%s/%s'%(directory,dataType,img['file_name']))
 # use url to load image
-I = io.imread(img['coco_url'])
+#I = io.imread(img['coco_url'])
 plt.axis('off')
 plt.imshow(I)
 plt.show()
@@ -42,3 +42,9 @@ plt.imshow(I); plt.axis('off')
 annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
 anns = coco.loadAnns(annIds)
 coco.showAnns(anns)
+
+
+
+
+
+#
