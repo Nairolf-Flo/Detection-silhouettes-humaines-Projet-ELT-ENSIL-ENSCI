430abd318461faf52d816628883d8786a22bc99f
Nairolf-Flo <florian.cadaux@etu.unilim.fr>
2021-03-07 10:28:52 +0100

    maj de preparation_pour_coco.sh

--- preparation_pour_coco.sh
+++ preparation_pour_coco.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
+echo "Update VM..."
 sudo apt-get update
 sudo apt-get upgrade
+echo "Update OK"
+echo ""
+echo "Installation de Python3.8..."
 sudo apt install python3.8
-sudo apt install python-pip
-pip install --upgrade pip
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+echo "Python3.8 OK"
+echo ""
+echo "Installation de pip3..."
+sudo apt install python3-pip
+pip3 install --upgrade pip
+echo "pip3 OK"
+echo ""
 echo "Installation de keras..."
 sudo pip3 install keras
-sudo pip3 install tensorflow==3.3
+echo "keras OK"
+echo ""
+echo "Installation de tensorflow >2.2..."
+sudo pip3 install tensorflow
+echo "tensorflow OK"
+echo ""
+echo "Installation de numpy..."
 sudo pip3 install numpy
-echo "OK"
-echo ""
-echo "Installation de Cython..."
-pip3 install Cython
-echo "OK"
-echo ""
-echo "Installation de matplotlib..."
-pip3 install matplotlib
-echo "OK"
-echo ""
-echo "Installation de API coco..."
-pip3 install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
-echo "OK"
+echo "numpy OK"
 echo ""
 echo "Installation de unzip..."
 sudo apt install zip unzip
+echo "unzip OK"
+echo ""
+echo "/!\ Installation pour COCO"
+echo "Installation de Cython..."
+pip3 install Cython
+echo "Cython OK"
+echo ""
+echo "Installation de matplotlib..."
+pip3 install matplotlib
+echo "Matplotlib OK"
+echo ""
+echo "Installation de API coco..."
+pip3 install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
 echo "OK"
 echo ""
 echo "Telechargement des annotations de coco..."
@@ -48,7 +64,7 @@ echo "Telechargement des images pour la validation..."
 wget http://images.cocodataset.org/zips/val2017.zip
 echo "OK"
 echo ""
-echo "Telechargement des images pour l'entrainement..."
+echo "Telechargement des images pour l entrainement..."
 wget http://images.cocodataset.org/zips/train2017.zip
 echo "OK"
 echo ""
