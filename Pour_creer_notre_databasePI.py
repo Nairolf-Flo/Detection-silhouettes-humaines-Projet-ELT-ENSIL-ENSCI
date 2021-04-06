7d7d08387b7513423cf53b13414ecfc26d510912
Nairolf-Flo <florian.cadaux@etu.unilim.fr>
2021-04-06 18:32:43 +0200

    ajout du # coding: utf-8

--- Pour_creer_notre_databasePI.py
+++ Pour_creer_notre_databasePI.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from pycocotools.coco import COCO
 from PIL import Image
 import os
--- setupKERAS_TF.sh
+++ setupKERAS_TF.sh
@@ -2,32 +2,20 @@
 echo "Update VM..."
 sudo apt-get update
 sudo apt-get upgrade
-echo "Update OK"
-echo ""
+
 echo "Installation de Python3.8..."
 sudo apt install python3.8
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-echo "Python3.8 OK"
-echo ""
+
 echo "Installation de pip3..."
 sudo apt install python3-pip
-pip3 install --upgrade pip
-echo "pip3 OK"
-echo ""
+sudo pip3 install --upgrade pip
+
 echo "Installation de keras..."
-sudo pip3 install keras
-echo "keras OK"
-echo ""
+pip3 install keras
+
 echo "Installation de tensorflow >2.2..."
-sudo pip3 install tensorflow
-echo "tensorflow OK"
-echo ""
+pip3 install tensorflow
+
 echo "Installation de numpy..."
-sudo pip3 install numpy
-echo "numpy OK"
-echo ""
-echo "Installation de unzip..."
-sudo apt install zip unzip
-echo "unzip OK"
-echo ""
-echo "La machinne est ready ! ^_^"
+pip3 install numpy
